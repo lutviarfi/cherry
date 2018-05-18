0350883fa1c8d624c812b9ab5876763224ab5526
lutviarfi <lutvi04@gmail.com>
2018-05-18 11:29:16 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Cherry Picking 001</title>
+</head>
+<body>
+	<h1>mouse</h1>
+	<h1>keyboard</h1>
+	<h1>Monitor</h1>
+	<h1>RAM</h1>
+	<h1>CPU</h1>
+	<h1>Open air filter</h1>
+</body>
+</html>
